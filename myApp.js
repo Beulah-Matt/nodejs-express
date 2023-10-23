898cdc9b9c8a24dceb0910c03b1cf11d6d480c74
Beulah Mathenge <beulahdaizy@gmail.com>
2023-10-23 21:39:06 +0300

    exercises 1 to 3

--- myApp.js
+++ myApp.js
@@ -1,11 +1,20 @@
 let express = require('express');
 let app = express();
 
+app.use('/public',express.static(__dirname + '/public') ) 
+/* exercise 1 */
+console.log("Hello World")
 
+/**exercise 2 */
+app.get('/', (req, res) => {
+    res.send("Hello Express")
+})
 
-
-
-
+/**exercise 3 */
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath);
+})
+ const absolutePath = __dirname + '/views/index.html'
 
 
 
